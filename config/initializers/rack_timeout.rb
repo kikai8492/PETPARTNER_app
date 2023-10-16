05e3cd115f49e6df051c1b2fc11aff3f6912e2b3
shuto kikai <yysskikai04@icloud.com>
2023-10-16 17:17:39 +0900

    racktime120に変更

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,1 @@
-Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 300
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 120
